15a369917b49faa7fcb9be744b835e2514b5ef5e
Aliaksei Chyrykau <L_chr93@mail.ru>
2023-08-06 23:31:57 +0300

    update: complited task

--- src/index.js
+++ src/index.js
@@ -1,78 +1,143 @@
 module.exports = function toReadable (number) {
-    let numbers = String(number).split("");
+    let numbers = String(Math.abs(number)).split("").reverse().join("");
     let numberText = "";
     for (let i = 0; i < numbers.length; i++) {
         if (i === 0) {
             switch (numbers[i]) {
                 case '0':
-                    numberText = numberText + 'zero';
+                    numberText = 'zero';
                     break;
                 case '1':
-                    numberText = numberText + 'one';
+                    numberText = 'one';
                     break;
                 case '2':
-                    numberText = numberText + 'two';
+                    numberText = 'two';
                     break;
                 case '3':
-                    numberText = numberText + 'three';
+                    numberText = 'three';
                     break;
                 case '4':
-                    numberText = numberText + 'four';
+                    numberText = 'four';
                     break;
                 case '5':
-                    numberText = numberText + 'five';
+                    numberText = 'five';
                     break;
                 case '6':
-                    numberText = numberText + 'six';
+                    numberText = 'six';
                     break;
                 case '7':
-                    numberText = numberText + 'seven';
+                    numberText = 'seven';
                     break;
                 case '8':
-                    numberText = numberText + 'eight';
+                    numberText = 'eight';
                     break;
                 case '9':
-                    numberText = numberText + 'nine';
+                    numberText = 'nine';
                     break;
             }
+            continue;
         }
 
-        if (i === 1) {
-            switch (numbers[i]) {
+        if (i === 1 && numbers[1] === '0') continue;
+
+        if (i === 1 && numbers[1] === '1') {
+            switch (numbers[0]) {
                 case '0':
-                    numberText = 'zero' + numberText;
+                    numberText = 'ten';
                     break;
                 case '1':
-                    numberText = numberText + 'one'
+                    numberText = 'eleven';
                     break;
                 case '2':
-                    numberText = numberText + 'two'
+                    numberText = 'twelve';
                     break;
                 case '3':
-                    numberText = numberText + 'three'
+                    numberText = 'thirteen';
                     break;
                 case '4':
-                    numberText = numberText + 'four'
+                    numberText = 'fourteen';
                     break;
                 case '5':
-                    numberText = numberText + 'five'
+                    numberText = 'fifteen';
                     break;
                 case '6':
-                    numberText = numberText + 'six'
+                    numberText = 'sixteen';
                     break;
                 case '7':
-                    numberText = numberText + 'seven'
+                    numberText = 'seventeen';
                     break;
                 case '8':
-                    numberText = numberText + 'eight'
+                    numberText = 'eighteen';
                     break;
                 case '9':
-                    numberText = numberText + 'nine'
+                    numberText = 'nineteen';
+                    break;
+            } 
+            continue;
+        } 
+        if (i === 1) {
+            if (numbers[0] === "0") numberText = "";
+            switch (numbers[i]) {
+                case '2':
+                    numberText = 'twenty ' + numberText;
+                    break;
+                case '3':
+                    numberText = 'thirty ' + numberText;
+                    break;
+                case '4':
+                    numberText = 'forty ' + numberText;
+                    break;
+                case '5':
+                    numberText = 'fifty ' + numberText;
+                    break;
+                case '6':
+                    numberText = 'sixty ' + numberText;
+                    break;
+                case '7':
+                    numberText = 'seventy ' + numberText;
+                    break;
+                case '8':
+                    numberText = 'eighty ' + numberText;
+                    break;
+                case '9':
+                    numberText = 'ninety ' + numberText;
                     break;
             }
         }
         
+        if (i === 2) {
+            if (numbers[0] === '0' && numbers[1] === '0') numberText = "";
+            switch (numbers[i]) {
+                case '1':
+                    numberText = 'one hundred ' + numberText;
+                    break;
+                case '2':
+                    numberText = 'two hundred ' + numberText;
+                    break;
+                case '3':
+                    numberText = 'three hundred ' + numberText;
+                    break;
+                case '4':
+                    numberText = 'four hundred ' + numberText;
+                    break;
+                case '5':
+                    numberText = 'five hundred ' + numberText;
+                    break;
+                case '6':
+                    numberText = 'six hundred ' + numberText;
+                    break;
+                case '7':
+                    numberText = 'seven hundred ' + numberText;
+                    break;
+                case '8':
+                    numberText = 'eight hundred ' + numberText;
+                    break;
+                case '9':
+                    numberText = 'nine hundred ' + numberText;
+                    break;
+            }
+        }
     }
 
-    return numberText;
+    return numberText.trim();
 }
