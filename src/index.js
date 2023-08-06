95037556d33b9dff754523a9662c70d688f98c79
Aliaksei Chyrykau <L_chr93@mail.ru>
2023-08-06 22:33:11 +0300

    fix: index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,42 @@ module.exports = function toReadable (number) {
         if (i === 0) {
             switch (numbers[i]) {
                 case '0':
-                    numberText = numberText + 'zero'
+                    numberText = numberText + 'zero';
+                    break;
+                case '1':
+                    numberText = numberText + 'one';
+                    break;
+                case '2':
+                    numberText = numberText + 'two';
+                    break;
+                case '3':
+                    numberText = numberText + 'three';
+                    break;
+                case '4':
+                    numberText = numberText + 'four';
+                    break;
+                case '5':
+                    numberText = numberText + 'five';
+                    break;
+                case '6':
+                    numberText = numberText + 'six';
+                    break;
+                case '7':
+                    numberText = numberText + 'seven';
+                    break;
+                case '8':
+                    numberText = numberText + 'eight';
+                    break;
+                case '9':
+                    numberText = numberText + 'nine';
+                    break;
+            }
+        }
+
+        if (i === 1) {
+            switch (numbers[i]) {
+                case '0':
+                    numberText = 'zero' + numberText;
                     break;
                 case '1':
                     numberText = numberText + 'one'
@@ -36,6 +71,7 @@ module.exports = function toReadable (number) {
                     break;
             }
         }
+        
     }
 
     return numberText;
